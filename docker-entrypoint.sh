af4dd05fdcb346eee652df5b349445445a784b74
Vitalii Sharavara <v.sharavara@gmail.com>
2016-09-23 18:55:12 +0700

    stable version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,12 @@
 serverID=/local/notesdata/server.id
 
 if [ ! -f "$serverID" ]; then
-    echo "This is a new server. Run remote setup"
-    exec su - notes -c "/opt/ibm/domino/bin/server –listen  &"
-    
+    #echo "This is a new server. Please run the command:"
+    #echo "su - notes -c \"cd /local/notesdata; /opt/ibm/domino/bin/server –listen \""
+    /opt/ibm/domino/bin/server -listen
 else
-    echo "Domino server start"
-    exec su - notes -c "/opt/ibm/domino/rc_domino_script start  &"
+    #echo "Domino server start"
+    #su - notes -c "/opt/ibm/domino/rc_domino_script start "
+    /opt/ibm/domino/rc_domino_script start
+    /bin/bash
 fi
